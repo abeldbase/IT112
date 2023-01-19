80a0daf5d72688492defd99057df734da63ccaac
ABEL MENGSTU <94520834+abeldbase@users.noreply.github.com>
2023-01-18 19:07:40 -0800

    updates

--- flask/app.py
+++ flask/app.py
@@ -17,6 +17,22 @@ def about():
 @app.route('/fortune', methods=['GET', 'POST'])
 def fortune():
     if request.method == 'POST':
+        color = request.form.get('color')
+        number = request.form.get('number')
+        if color == 'Yellow':
+
+            if number == 1:
+                return f"Hola {request.form.get('user')} You are very lucky!! Happy today!"
+
+            elif number == 2:
+                return f"Hola {request.form.get('user')} You will become a reach person  soon!"
+            elif number == 3:
+                return f"Hola {request.form.get('user')}You will get a new job next year!"
+
+            elif number == 4:
+                return f"Hola {request.form.get('user')}You will visit Paris after 2 years!"
+            else:
+                print("Numbers - 1, 2, 3, 4 are the only numbers allowed!")
         return f"Hi {request.form.get('name')}. You have selected {request.form.get('color')} &  {request.form.get('number')}"
     return render_template('fortune_form.html')
 
